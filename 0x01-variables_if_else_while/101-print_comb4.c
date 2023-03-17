922c4c4826d6f3a881db9a67afa9aa554fa4db24
yassineDevox <yassine.rassy1@gmail.com>
2023-03-17 11:55:35 -0700

    fix betty's errors

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -4,24 +4,26 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
-		int i,j,k;
-		
-		
-		for(i=0;i<8;i++)
+	int i, j, k;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
 		{
-			for(j=i+1;j<=9;j++)
+			for (k = j + 1; k <= 9; k++)
 			{
-				for(k=j+1;k<=9;k++)
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+				if (i != 7)
 				{
-					putchar(i+'0');
-					putchar(j+'0');
-					putchar(k+'0');
-					if(i!=7)
-					putchar(' ,');
+					putchar(44);
+					putchar(32);
 				}
 			}
 		}
+	}
 	return (0);
 }
